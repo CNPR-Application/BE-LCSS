c851224a88db7ea547e2cebd936bdedbe38cc2ff
Huynh Lam <lamhntse62917@fpt.edu.vn>
2021-06-04 03:17:48 +0700

    [LamHNT] first commit

---
+++ src/main/java/cnpr/lcss/LCSSApplication.java
@@ -0,0 +1,13 @@
+package cnpr.lcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LCSSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LCSSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cnpr/lcss/LCSSApplicationTests.java
@@ -0,0 +1,13 @@
+package cnpr.lcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LCSSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
