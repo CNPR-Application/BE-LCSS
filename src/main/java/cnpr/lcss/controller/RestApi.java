e015ad4dc70997749b98d7704ce77516ba1e2088
Huynh Lam <lamhntse62917@fpt.edu.vn>
2021-06-14 14:42:50 +0700

    [LamHNT] Change CORs annotation

--- src/main/java/cnpr/lcss/controller/RestApi.java
+++ src/main/java/cnpr/lcss/controller/RestApi.java
@@ -21,6 +21,7 @@ public class RestApi {
      * @return
      * @apiNote welcome page
      */
+    @CrossOrigin
     @RequestMapping(value = "/")
     public String welcome() {
         return "Welcome to LCSS - Language Center Support System!";
@@ -33,7 +34,7 @@ public class RestApi {
      * @apiNote 1.0-check-login
      * @author LamHNT - 2021.06.03
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<?> checkLogin(@RequestBody LoginRequestDto loginRequestDto) throws Exception {
         return accountService.checkLogin(loginRequestDto);
@@ -47,7 +48,7 @@ public class RestApi {
      * @apiNote 13.0-search-curriculum-by-curriculum-name
      * @author LamHNT - 2021.06.08
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums", params = "name", method = RequestMethod.GET)
     public CurriculumPagingResponseDto searchCurriculumByName(@RequestParam(value = "name") String keyword,
                                                               @RequestParam(value = "pageNo") int pageNo,
@@ -64,7 +65,7 @@ public class RestApi {
      * @apiNote 14.0-search-curriculum-by-curriculum-code
      * @author LamHNT - 2021.06.08
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums", params = "code", method = RequestMethod.GET)
     public CurriculumPagingResponseDto searchCurriculumByCode(@RequestParam(value = "code") String keyword,
                                                               @RequestParam(value = "pageNo") int pageNo,
@@ -80,7 +81,7 @@ public class RestApi {
      * @apiNote 15.0-get-curriculum-details-by-curriculum-id
      * @author LamHNT - 2021.06.09
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.GET)
     public ResponseEntity<?> getCurriculumDetails(@PathVariable int curriculumId) throws Exception {
         return curriculumService.findOneByCurriculumId(curriculumId);
@@ -93,7 +94,7 @@ public class RestApi {
      * @apiNote 16.0-delete-curriculum-by-curriculum-id
      * @author LamHNT - 2021.06.10
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteCurriculumByCurriculumId(@PathVariable int curriculumId) throws Exception {
         return curriculumService.deleteByCurriculumId(curriculumId);
@@ -106,7 +107,7 @@ public class RestApi {
      * @apiNote 17.0-create-curriculum
      * @author LamHNT - 2021.06.11
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums", method = RequestMethod.POST)
     public ResponseEntity<?> createNewCurriculum(@RequestBody CurriculumRequestDto newCur) throws Exception {
         return curriculumService.createNewCurriculum(newCur);
@@ -120,7 +121,7 @@ public class RestApi {
      * @apiNote 18.0-edit-curriculum-by-curriculum-id
      * @author LamHNT - 2021.06.12
      */
-    @CrossOrigin(origins = "http://localhost:4200")
+    @CrossOrigin
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.PUT)
     public ResponseEntity<?> updateCurriculumByCurriculumId(@PathVariable int curriculumId,
                                                             @RequestBody CurriculumRequestDto insCur) throws Exception {
