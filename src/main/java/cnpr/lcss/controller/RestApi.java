a2706d4c5f9c74d0dc181bc930530b7936fe2ab5
Huynh Lam <lamhntse62917@fpt.edu.vn>
2021-06-14 14:35:19 +0700

    [LamHNT] Add CORs for all API

--- src/main/java/cnpr/lcss/controller/RestApi.java
+++ src/main/java/cnpr/lcss/controller/RestApi.java
@@ -47,6 +47,7 @@ public class RestApi {
      * @apiNote 13.0-search-curriculum-by-curriculum-name
      * @author LamHNT - 2021.06.08
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums", params = "name", method = RequestMethod.GET)
     public CurriculumPagingResponseDto searchCurriculumByName(@RequestParam(value = "name") String keyword,
                                                               @RequestParam(value = "pageNo") int pageNo,
@@ -63,6 +64,7 @@ public class RestApi {
      * @apiNote 14.0-search-curriculum-by-curriculum-code
      * @author LamHNT - 2021.06.08
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums", params = "code", method = RequestMethod.GET)
     public CurriculumPagingResponseDto searchCurriculumByCode(@RequestParam(value = "code") String keyword,
                                                               @RequestParam(value = "pageNo") int pageNo,
@@ -78,6 +80,7 @@ public class RestApi {
      * @apiNote 15.0-get-curriculum-details-by-curriculum-id
      * @author LamHNT - 2021.06.09
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.GET)
     public ResponseEntity<?> getCurriculumDetails(@PathVariable int curriculumId) throws Exception {
         return curriculumService.findOneByCurriculumId(curriculumId);
@@ -90,6 +93,7 @@ public class RestApi {
      * @apiNote 16.0-delete-curriculum-by-curriculum-id
      * @author LamHNT - 2021.06.10
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteCurriculumByCurriculumId(@PathVariable int curriculumId) throws Exception {
         return curriculumService.deleteByCurriculumId(curriculumId);
@@ -102,6 +106,7 @@ public class RestApi {
      * @apiNote 17.0-create-curriculum
      * @author LamHNT - 2021.06.11
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums", method = RequestMethod.POST)
     public ResponseEntity<?> createNewCurriculum(@RequestBody CurriculumRequestDto newCur) throws Exception {
         return curriculumService.createNewCurriculum(newCur);
@@ -115,6 +120,7 @@ public class RestApi {
      * @apiNote 18.0-edit-curriculum-by-curriculum-id
      * @author LamHNT - 2021.06.12
      */
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/curriculums/{curriculumId}", method = RequestMethod.PUT)
     public ResponseEntity<?> updateCurriculumByCurriculumId(@PathVariable int curriculumId,
                                                             @RequestBody CurriculumRequestDto insCur) throws Exception {
