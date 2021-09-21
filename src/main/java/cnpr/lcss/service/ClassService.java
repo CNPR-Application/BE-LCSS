7e94139b18d33500ce08665c25e43bf3a4c60f81
ahuuwx <huuntse130770@fpt.edu.vn>
2021-09-21 17:08:20 +0700

    [HuuNT] order by openingDate DESC

--- src/main/java/cnpr/lcss/service/ClassService.java
+++ src/main/java/cnpr/lcss/service/ClassService.java
@@ -361,7 +361,7 @@ public class ClassService {
                 }
 
                 //Get classes with CLASSID LIST and STATUS
-                Page<Class> classList = classRepository.findClassByClassIdIsInAndStatus(list, status, pageable);
+                Page<Class> classList = classRepository.findClassByClassIdIsInAndStatusOrderByOpeningDateDesc(list, status, pageable);
                 List<ClassTeacherSearchDto> classSearchDtoList = classList.getContent().stream().map(aClass -> aClass.convertToTeacherSearchDto()).collect(Collectors.toList());
                 int pageTotal = classList.getTotalPages();
                 for (ClassTeacherSearchDto aClass : classSearchDtoList) {
