7f6bf4f08b43c18476335e1fc504d66df5f5b197
lamdwzrd <lamhntse62917@fpt.edu.vn>
2021-12-05 18:07:50 +0700

    [LamHNT] edit authenticated url

--- src/main/java/cnpr/lcss/security/SecurityConfigurer.java
+++ src/main/java/cnpr/lcss/security/SecurityConfigurer.java
@@ -44,8 +44,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(Constant.URL_BRANCH, Constant.URL_CLASS, Constant.URL_CLASS_FILTER,
                         Constant.URL_CURRICULUM, Constant.URL_FORGOT_PASSWORD, Constant.URL_GUESTS,
-                        Constant.URL_LOGIN, Constant.URL_SHIFT, Constant.URL_SUBJECT, Constant.URL_SUBJECT_DETAIL,
-                        "/accounts", "/notification-to-group")
+                        Constant.URL_LOGIN, Constant.URL_SHIFT, Constant.URL_SUBJECT, Constant.URL_SUBJECT_DETAIL)
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
