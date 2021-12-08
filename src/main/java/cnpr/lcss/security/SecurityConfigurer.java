797962c122b5814a24281f158c975c6f085175fd
lamdwzrd <lamhntse62917@fpt.edu.vn>
2021-12-08 21:57:23 +0700

    [LamHNT] fix authen

--- src/main/java/cnpr/lcss/security/SecurityConfigurer.java
+++ src/main/java/cnpr/lcss/security/SecurityConfigurer.java
@@ -44,7 +44,9 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(Constant.URL_BRANCH, Constant.URL_CLASS, Constant.URL_CLASS_FILTER,
                         Constant.URL_CURRICULUM, Constant.URL_FORGOT_PASSWORD, Constant.URL_GUESTS,
-                        Constant.URL_LOGIN, Constant.URL_SHIFT, Constant.URL_SUBJECT, Constant.URL_SUBJECT_DETAIL)
+                        Constant.URL_LOGIN, Constant.URL_SHIFT, Constant.URL_SUBJECT, Constant.URL_SUBJECT_DETAIL,
+                        "/notification-to-group", "/notification-to-person", "/notification-to-all",
+                        "/notification-in-class", "/notification-to-staff")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
